6b75246549ae743703a222f4d2c00cfc2c9dcff0
Philipp Balzarek <p.balzarek@googlemail.com>
2019-04-11 19:34:24 +0200

    Add occur-dwim

--- init.d/progmode.el
+++ init.d/progmode.el
@@ -1,1 +1,31 @@
 (add-hook 'prog-mode-hook 'fci-mode)
+
+(defun occur-dwim ()
+  "Call occur on word at point (if exists)"
+  (interactive)
+  (let* ((here (if (region-active-p)
+                   (buffer-substring-no-properties
+                    (region-beginning)
+                    (region-end))
+                 (thing-at-point 'word) (thing-at-point 'word))))
+    (if here
+        (funcall-interactively #'occur here)
+        (call-interactively #'occur))))
+
+;; (defun occur-dwim ()
+;;   "Call `occur' with a sane default."
+;;   (interactive)
+;;   (push (if (region-active-p)
+;;             (buffer-substring-no-properties
+;;              (region-beginning)
+;;              (region-end))
+;;           (let ((sym (thing-at-point 'symbol)))
+;;             (when (stringp sym)
+;;               (regexp-quote sym))))
+;;         regexp-history)
+;;   (call-interactively 'occur))
+
+(define-key occur-mode-map (kbd "n") #'occur-next)
+(define-key occur-mode-map (kbd "p") #'occur-prev)
+
+(define-key prog-mode-map (kbd "C-c o") #'occur-dwim)
