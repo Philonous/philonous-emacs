1fde971924eb90c885e973bdc60177bc57788e51
Philipp Balzarek <p.balzarek@googlemail.com>
2019-10-02 15:22:36 +0200

    Consolidate html and js configuration

--- init.d/web-init.el
+++ init.d/web-init.el
@@ -1,3 +1,5 @@
+;;; JS
+
 (require 'compile)
 (require 'subr-x)
 
@@ -107,3 +109,22 @@
   )
 
 (add-hook 'rjsx-mode-hook #'rjsx-mode-init)
+
+
+;;; HTML
+
+(use-package company-tern
+  :config
+  (add-to-list 'company-backends 'company-tern))
+
+(use-package tagedit
+  :config
+  (tagedit-add-paredit-like-keybindings)
+  (tagedit-add-experimental-features)
+  :hook (html-mode . tagedit-mode))
+
+(setq sgml-quick-keys 'close)
+
+;;; Web mode
+(use-package web-mode
+  )
