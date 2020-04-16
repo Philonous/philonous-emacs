27a0ed47c42793acb2310de1c7c1352fd93a9cbc
Philipp Balzarek <p.balzarek@googlemail.com>
2020-04-16 10:39:42 +0200

    Fix SQL interactive mode not recognizing prompt

--- init.d/sql-mode.el
+++ init.d/sql-mode.el
@@ -3,7 +3,8 @@
 (defun custom-sql-interactive-mode-hook ()
     (define-key sql-interactive-mode-map (kbd "C-c C-k")
       'comint-clear-interactive-buffer )
-    )
+    (sql-set-product-feature 'postgres :prompt-regexp "^[-[:alnum:]_]*=[#>] ")
+    (sql-set-product-feature 'postgres :prompt-cont-regexp "^[-[:alnum:]_]*[-(][#>] "))
 
 
-(add-hook 'sql-interactive-mode-hook 'custom-sql-interactive-mode-hook)
+(add-hook 'sql-mode-hook 'custom-sql-interactive-mode-hook)
